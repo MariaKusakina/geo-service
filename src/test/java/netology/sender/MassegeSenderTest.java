72a0b92961c87c455a635543755a23526e19806c
MariaKusakina <m.a.kusakina@gmail.com>
2022-07-09 16:15:34 +0300

    added changes in project

---
+++ src/test/java/netology/sender/MassegeSenderTest.java
@@ -0,0 +1,39 @@
+package netology.sender;
+
+import org.junit.jupiter.api.Assertions;
+import org.mockito.Mockito;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+import ru.netology.geo.GeoService;
+import ru.netology.i18n.LocalizationService;
+import ru.netology.sender.MessageSenderImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class MessageSenderTest {
+
+    @org.junit.jupiter.api.Test
+    void testMessageSenderRus() {
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.0.32.11");
+        GeoService geoService = Mockito.mock(GeoService.class);
+        Mockito.when(geoService.byIp(Mockito.eq("172.0.32.11"))).thenReturn(new Location(null, Country.RUSSIA, null, 0));
+        LocalizationService localizationService = Mockito.mock(LocalizationService.class);
+        Mockito.when(localizationService.locale(Mockito.eq(Country.RUSSIA))).thenReturn("Добро пожаловать");
+        MessageSenderImpl messageSender = new MessageSenderImpl(geoService, localizationService);
+        Assertions.assertEquals("Добро пожаловать", messageSender.send(headers));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testMessageSenderUSA() {
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "96.44.183.149");
+        GeoService geoService = Mockito.mock(GeoService.class);
+        Mockito.when(geoService.byIp(Mockito.eq("96.44.183.149"))).thenReturn(new Location(null, Country.USA, null, 0));
+        LocalizationService localizationService = Mockito.mock(LocalizationService.class);
+        Mockito.when(localizationService.locale(Mockito.eq(Country.USA))).thenReturn("Welcome");
+        MessageSenderImpl messageSender = new MessageSenderImpl(geoService, localizationService);
+        Assertions.assertEquals("Welcome", messageSender.send(headers));
+    }
+}
